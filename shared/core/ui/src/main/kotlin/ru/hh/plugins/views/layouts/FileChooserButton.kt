b3c6423cff3258a41ad1e9e336e88511b9ab3b40
Daniil Timonin <rand1kch@gmail.com>
2024-10-23 16:25:48 +0400

    update version studio

--- shared/core/ui/src/main/kotlin/ru/hh/plugins/views/layouts/FileChooserButton.kt
+++ shared/core/ui/src/main/kotlin/ru/hh/plugins/views/layouts/FileChooserButton.kt
@@ -16,25 +16,4 @@ import javax.swing.filechooser.FileNameExtensionFilter
  * @param filterFilesExtensions -- extensions for filtering your files (without ".", e.g. "yaml")
  * @param approveAction -- action that will be invoked, if user choose something
  */
-fun Row.fileChooserButton(
-    project: Project,
-    buttonText: String,
-    filterText: String,
-    fileChooserButtonText: String,
-    vararg filterFilesExtensions: String,
-    approveAction: (File) -> Unit
-) {
-    button(buttonText) {
-        val fileChooser = JFileChooser().apply {
-            isMultiSelectionEnabled = false
-            fileSelectionMode = JFileChooser.FILES_ONLY
-            fileFilter = FileNameExtensionFilter(filterText, *filterFilesExtensions)
-            project.basePath?.let { currentDirectory = File(it) }
-        }
 
-        val result = fileChooser.showDialog(null, fileChooserButtonText)
-        if (result == JFileChooser.APPROVE_OPTION) {
-            approveAction.invoke(fileChooser.selectedFile)
-        }
-    }
-}
